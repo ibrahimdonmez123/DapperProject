694c130146624d328b0567c372b5a76a7dc76246
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 20:48:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=DESKTOP-JI1JKUA\\SQLEXPRESS;Database=Employees;Trusted_Connection=true";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            var employees = connection.Query<Employee>("SELECT * FROM Employee");
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.Id}, Name: {employee.Name}, Salary: {employee.Salary}");
+            }
+
+            var specificEmployee = connection.QueryFirstOrDefault<Employee>("SELECT * FROM Employee WHERE Id = @Id", new { Id = 1 });
+
+
+            connection.Close();
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public decimal Salary { get; set; } = 0.0m;
+    }
+
+}
+
